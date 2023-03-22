e11442560cf955a49e434464f24e1fb7ad4623ef
Olga Pikulska <olga.pikulska.ld@gmail.com>
2023-03-23 00:19:31 +0100

    Create basic pages components

---
+++ src/pages/Phonebook.js
@@ -0,0 +1,17 @@
+//import { useDispatch } from "react-redux";
+//import { useEffect } from "react";
+//import { fetchTasks } from "../redux/tasks/tasks.thunk";
+
+
+export const Phonebook = () => {
+    // const dispatch = useDispatch()
+    // useEffect(() => {
+    //     dispatch(fetchTasks('arg'))
+    // }, [])
+
+    return (
+        <>
+
+        </>
+    );
+};
