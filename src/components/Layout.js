66781c838adb44dfdb60df8f96d3f99a75a4a927
Olga Pikulska <olga.pikulska.ld@gmail.com>
2023-03-23 00:25:00 +0100

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { Outlet } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.main`
+display: grid;
+  gap: 16px;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 16px;
+`
+const Nav = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 50px;
+  border-bottom: 2px solid black;
+  margin-bottom: 100px;
+  padding-right: 50px;
+`
+export const Layout = () => {
+    const navigate = useNavigate()
+    return <Container>
+        <Nav>
+            <button onClick={() => navigate('sign-in')}>Sign in</button>
+            <button onClick={() => navigate('register')}>Register</button>
+            <button onClick={() => navigate('')}>Sign out</button>
+        </Nav>
+        <Outlet />
+    </Container>;
+};
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
